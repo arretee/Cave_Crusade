74ebef891dc51a462e4bfce7b925557fccce5d36
arretee <arreteear@gmail.com>
2023-03-24 19:50:14 +0300

    1

--- code/settings.py
+++ code/settings.py
@@ -1,1 +1,34 @@
 screen_resolutions = [[1280, 720], [1600, 900], [1920, 1080]]
+
+pathes = {
+    "character": {
+        "axe": {
+            "attack": "../graphics/characters/basic/axe/attack",
+            "idle": "../graphics/characters/basic/axe/idle",
+            "run": "../graphics/characters/basic/axe/run",
+        },
+        "basic": {
+            "climb": "../graphics/characters/basic/basic/climb",
+            "idle": "../graphics/characters/basic/basic/idle",
+            "jump": "../graphics/characters/basic/basic/jump",
+            "run": "../graphics/characters/basic/basic/run",
+        },
+        "bow": {
+            "attack": "../graphics/characters/basic/bow/attack",
+            "idle": "../graphics/characters/basic/bow/idle",
+            "run": "../graphics/characters/basic/bow/run",
+        },
+        "pique": {
+            "attack": "../graphics/characters/basic/pique/attack",
+            "idle": "../graphics/characters/basic/pique/idle",
+            "run": "../graphics/characters/basic/pique/run",
+        },
+        "sword": {
+            "attack": "../graphics/characters/basic/sword/attack",
+            "idle": "../graphics/characters/basic/sword/idle",
+            "run": "../graphics/characters/basic/sword/run",
+        },
+    },
+    "enemy": {},
+    "items": {},
+}
